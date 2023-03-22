38f1fa587b4036bd7107f65b63c1ab918b70cc74
rhtkdgns <47663167+rhtkdgns@users.noreply.github.com>
2023-03-22 16:39:22 +0900

    20230322_update

--- project/src/component/MovieForm.js
+++ project/src/component/MovieForm.js
@@ -1,19 +1,44 @@
+/* eslint-disable */
 import {useState} from 'react';
 const MovieForm = ({addMovie}) => {
     const [moviesTitle, setMovieTitle] = useState("");
     const [moviesYear, setMovieYear] = useState("");
+    const [titleError, setTitleError] = useState('');
+    const [yearError, setYearError] = useState('');
 
     const reset = () => {
         setMovieTitle('');
         setMovieYear('');
     }
+    
+    const validateForm = () => {
+        let validated = true;
+        if(!moviesTitle){
+            setTitleError('title이 비어있습니다.');
+            validated = false;
+        }else{
+            setTitleError('');
+        }
+        
+        if(!moviesYear){
+            setYearError('year가 비어있습니다.');
+            validated = false;
+        }else{
+            setYearError('');
+        }
+
+        return validated;
+    }
 
     const submit = (e) => {
         e.preventDefault(); // page 리로딩 방지
-        addMovie({
-            title:moviesTitle,
-            year:moviesYear,
-        });
+        if(validateForm()){
+            addMovie({
+                id:Date.now(),
+                title:moviesTitle,
+                year:moviesYear,
+            });
+        }
         reset();
       }
     return (
@@ -24,12 +49,14 @@ const MovieForm = ({addMovie}) => {
             placeholder='영화제목'
             onChange={e => setMovieTitle(e.target.value)}/>
             <br/>
+            <div style={{color: 'red'}}>{titleError}</div>
             <input
-            type='text'
+            type='number'
             value={moviesYear}
             placeholder='영화년도'
             onChange={e=> setMovieYear(e.target.value)}
             />
+            <div style={{color: 'red'}}>{yearError}</div>
             <button type='submit'>추가</button>
             <br/>
         </form>
