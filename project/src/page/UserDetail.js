3db9f83aa9907d66e70ee6b5a9132d6db25b9339
rhtkdgns <47663167+rhtkdgns@users.noreply.github.com>
2023-03-31 13:50:51 +0900

    test finish

--- project/src/page/UserDetail.js
+++ project/src/page/UserDetail.js
@@ -1,7 +1,26 @@
+/* eslint-disable */
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Spinner from '../component/Spinner';
+
 const UserDetail = () => {
+    const [load, setLoad] = useState('true');
+    const [user, setUser] = useState([]);
+    const {id} = useParams();
+    useEffect(()=> {
+        axios.get('https://jsonplaceholder.typicode.com/users/'+ id)
+        .then((res) => {
+            console.log(res.data);
+            setUser(res.data);
+            setLoad('false');
+        })
+    },[]);
+
+    
     return(
         <>
-            <div><h1>UserDetail</h1></div>
+            {load ? <div>{user.website}</div>:<Spinner/>}
         </>   
     )
 }
