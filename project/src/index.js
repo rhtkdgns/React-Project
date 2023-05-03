fa368c82e3e7a7b3ddc4446b508606c108e67816
rhtkdgns <47663167+rhtkdgns@users.noreply.github.com>
2023-05-03 14:36:39 +0900

    lastpaing

--- project/src/index.js
+++ project/src/index.js
@@ -3,17 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- project/src/page/Home.js
+++ project/src/page/Home.js
@@ -1,26 +1,9 @@
 /* eslint-disable */
-import { useSelector, useDispatch } from "react-redux";
-import { increseCount } from "reducers/count";
-
-// dispatch를 사용하기 위한 준비
-const dispatch = useDispatch();
-
-// store에 접근하여 state 가져오기
-const { count } = useSelector(state => state.counter);
-
-const increse = () => {
-  // store에 있는 state 바꾸는 함수 실행
-  dispatch(increseCount());
-};
 const Home = () =>{
     return(
         <>
             <h1>HOME</h1>
-            <div>
-                {count}
-                <button onClick={increse}>증가</button>
-            </div>
         </>
     )
 }
-export default Home ;
+export default Home;
